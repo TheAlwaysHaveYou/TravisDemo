9d9abe9b143273bcf844b3d6486508d8434093bf
范魁东 <fankuidong@fankuidongdeMacBook-Pro.local>
2019-11-26 15:52:47 +0800

    重建

---
+++ TravisDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TravisDemo
+//
+//  Created by 范魁东 on 2019/11/26.
+//  Copyright © 2019 FanKD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
